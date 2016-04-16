406f55d837d345a375593f3da481bd888d08fcc1
Kelsey James <kelseybjames@gmail.com>
2016-04-16 13:45:39 -0700

    more controller stuff

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,31 +1,38 @@
 class StoriesController < ApplicationController
 
-  def index
+  before_action :set_writer
 
+  def index
+    @stories = @writer.stories
   end
 
   def new
-
+    @story = Story.new
   end
 
   def create
-
+    
   end
 
   def show
-
+    @story = Story.find(params[:id])
   end
 
   def edit
-
+    @story = Story.find(params[:id])
   end
 
   def update
+    @story = Story.find(params[:id])
+    if @story.update
+      redirect_to writer_story_path(@writer, @story)
+    else
 
+    end
   end
 
   def destroy
-
+    @story = Story.find(params[:id])
   end
 
   private
