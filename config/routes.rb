3ef8dade54d99bf2ac6d00869d806b1a0d9e6a83
Kelsey James <kelseybjames@gmail.com>
2016-04-30 14:56:51 -0700

    quick addition to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :writers, only: [:index, :new, :create, :show, :edit, :update] do
     resources :stories
     resources :goals
+    resources :writer_goals
   end
   root to: 'writers#new'
 end
