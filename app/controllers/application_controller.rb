031b27f26fe31bb178c8be0e4e6a66c948787329
Veronica Cannon <me@veronicacannon.com>
2016-06-22 18:03:18 -0700

    Basic formating for writer pages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!, except: [:show]
-  before_filter :current_user_or_admin?, only: [:edit, :update]
+  before_filter :current_user_or_admin?, only: [:edit, :update, :wall]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
